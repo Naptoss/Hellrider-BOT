cbbc1533d121963cdf9e231018f44b5c6a9c9356
Naptoss <tomgabinio@gmail.com>
2024-08-05 13:03:09 -0300

    subindo teste do webhoow 2

--- my_bot/commands/ajuda.py
+++ my_bot/commands/ajuda.py
@@ -7,5 +7,6 @@ async def ajuda(ctx):
         "- `!buscar_membro [passaporte]`: Use este comando para buscar as atividades de farm de um membro específico pelo passaporte. Se você não fornecer um passaporte, o bot mostrará uma lista de todos os membros registrados para você escolher.\n"
         "- `!consultar`: Use este comando para ver todos os registros de farm associados ao seu passaporte. O bot enviará as informações diretamente para suas mensagens diretas.\n"
         "- `!ajuda`: Exibe esta lista de comandos e explica como usar cada um deles."
+        "Teste"
     )
     await ctx.send(help_message)
